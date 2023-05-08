86a48cbab8469d993e847fb191fd854c1f2ddac3
oliviachef <116026363+oliviachef@users.noreply.github.com>
2023-05-08 15:05:30 +0530

    commit

--- rescue/tdh-token.sol
+++ rescue/tdh-token.sol
@@ -1,1 +1,9 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract TDH is ERC20, Ownable {
+    constructor() ERC20("TDH", "TDH") {
+        _mint(msg.sender, 10000000 * 10**18);
+    }
+}
